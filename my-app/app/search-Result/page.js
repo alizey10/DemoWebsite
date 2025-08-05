21914d7e5fced94759b6ac028ae78e0e76110ae4
alizey10 <aleezaift09@gmail.com>
2025-08-05 19:40:48 +0500

    added file

--- my-app/app/search-Result/page.js
+++ my-app/app/search-Result/page.js
@@ -1,43 +1,15 @@
-"use client";
-import Card from "../card/page";
-import axios from "axios";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense } from "react";
+import Products from "./prodct";
 
+export default function SearchResultPage() {
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>Search Results</h2>
 
-export default function Products(){
-    const [products,setProducts]=useState([]);
-    let params= useSearchParams();
-    let [load ,setLoad]=useState("");
-    let userSearch=params.get('search');
-
-    useEffect(()=>{
-        setLoad("loading")
-        axios.get('/api/products?search='+ userSearch ).then((resp)=>{
-            setProducts(resp.data.products);
-            setLoad('loaded')
-        })
-    },[])
     
-     return  <div className="product-container">
-
-        {<div style={{display:"flex",flexWrap:"wrap",justifyContent:"space-around"}}>
-          {products.map((product, i) => (
-          <div key={i}  >
-             <Card  product={product}  />
-
-  {
-            load =="loaded" && !products.length && <div class="text-center text-danger">
-                No search results found
-            </div>
-            }
-          </div>
-        ))}
-           </div>  
-        }
-
+      <Suspense fallback={<div style={{ textAlign: "center" }}>Loading search results...</div>}>
+        <Products />
+      </Suspense>
     </div>
-
-           
-       
+  );
 }
